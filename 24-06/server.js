9d23a8a72869568709c94ac09256fab724a231b2
Sophie <sophie.ischenko@techstarter.de>
2025-01-07 14:33:58 +0100

    Update server.js

--- 24-06/server.js
+++ 24-06/server.js
@@ -8,7 +8,11 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // Dummy-Datenbank
-let books = [];
+let books = [
+    { id: 1, title: "1984", author: "George Orwell", year: 1949 },
+    { id: 2, title: "Brave New World", author: "Aldous Huxley", year: 1932 },
+    { id: 3, title: "Fahrenheit 451", author: "Ray Bradbury", year: 1953 },
+];
 
 // Endpunkte
 
